ae853a4178ac32cc4409a2da4deb6a02872f9c46
screaloal <belloayomipo575@gmail.com>
2026-08-15 21:28:41 +0100

    Fix PrismaClient v7 initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
